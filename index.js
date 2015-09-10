12c3ea22adee7af546df4640773d1e0fec760390
Jason Orendorff <jason.orendorff@gmail.com>
2015-09-10 15:00:18 -0500

    Movement using the arrow keys, home and end. Goal column does not persist across blank lines, both to keep it simple and for rhetorical purposes.

--- index.js
+++ index.js
@@ -24,6 +24,84 @@ var text = "";       // Full text of the document.
 var cursors = {};    // Maps user ids to integer offsets within the document.
 var nextUserId = 0;  // Used to generate a unique id for each user.
 
+// Now a function to compute movement within the document. We'll use this when
+// a user hits an arrow key.
+//
+// This is where the cost of having a single string instead of an array of
+// lines becomes obvious -- but don't rush to change it: an array of lines
+// would have its own problems!
+function computeMove(text, offset, dir) {
+  // Return the offset of the first character in the line of `text` that
+  // contains the given `offset`. So if `offset` is already at the beginning
+  // of a line, this returns it unchanged.
+  function lineStart(offset) {
+    while (offset > 0 && text[offset - 1] !== '\n')
+      offset--;
+    return offset;
+  }
+
+  // Return the offset of the end of the line that contains `offset`.
+  // The result will point to a newline or else the end of the document.
+  function lineEnd(offset) {
+    while (offset < text.length && text[offset] !== '\n')
+      offset++;
+    return offset;
+  }
+
+  // Return the offset of the line after the one containing `text[offset]`,
+  // or undefined if there is no next line.
+  function nextLineStart(offset) {
+    for (; offset < text.length; offset++) {
+      if (text[offset] === '\n')
+        return offset + 1;
+    }
+    return undefined; // hit end of document: no more lines
+  }
+
+  switch (dir) {
+    case "left":
+      if (offset === 0)
+        return undefined;  // already at top left corner; nowhere to go
+      return offset - 1;
+
+    case "right":
+      if (offset === text.length)
+        return undefined;  // already at end of document; nowhere to go
+      return offset + 1;
+
+    case "home": {
+      var start = lineStart(offset);
+      return start === offset ? undefined : start;
+    }
+
+    case "end": {
+      var end = lineEnd(offset);
+      return end === offset ? undefined : end;
+    }
+
+    case "up": {
+      var l1 = lineStart(offset);
+      if (l1 === 0)
+        return undefined;  // already on the first line; nowhere to go
+      var goalColumn = offset - l1;
+      var l0 = lineStart(l1 - 1);
+      return Math.min(l0 + goalColumn, l1 - 1);
+    }
+
+    case "down": {
+      var goalColumn = offset - lineStart(offset);
+      var l1 = nextLineStart(offset);
+      if (l1 === undefined)
+        return undefined; // already on the last line; nowhere to go
+      return Math.min(l1 + goalColumn, lineEnd(l1));
+    }
+
+    default:
+      console.log("unrecognized move code:", dir);
+      return undefined;
+  }
+}
+
 // Now all we have to do is handle socket.io connections so people can interact
 // with the document. For example, every time a user connects:
 io.on('connection', function (socket) {
@@ -31,10 +109,10 @@ io.on('connection', function (socket) {
   var userId = nextUserId++;
 
   // When this user types a character...
-  socket.on('typed', ch => {
+  socket.on('type', ch => {
     console.log("User " + userId + " typed character `" + ch + "`");
 
-    // ...go ahead and put it in the document...
+    // ...put it in the document...
     var offset = cursors[userId];
     text = text.slice(0, offset) + ch + text.slice(offset, text.length);
 
@@ -48,7 +126,19 @@ io.on('connection', function (socket) {
     // Notify everyone (including this user!) that a character was typed. Even
     // the user who hit the key doesn't see the character until their browser
     // receives this message.
-    io.emit('insert', {ch: ch, offset: offset, user: userId});
+    io.emit('typed', {ch: ch, offset: offset, user: userId});
+  });
+
+  // When the user hits an arrow key...
+  socket.on('move', dir => {
+    // ...figure out if the move is actually possible, and if so, where we're
+    // moving to...
+    var newOffset = computeMove(text, cursors[userId], dir);
+    if (newOffset !== undefined) {
+      // ...then update the model and notify all clients.
+      cursors[userId] = newOffset;
+      io.emit('moved', {user: userId, offset: newOffset});
+    }
   });
 
   // When this user disconnects, delete their cursor.
@@ -56,11 +146,11 @@ io.on('connection', function (socket) {
     delete cursors[userId];
 
     // Also make the cursor disappear from everyone's screen.
-    io.emit('user.exit', userId);
+    io.emit('disconnected', userId);
   });
 
   // OK. Since this user just connected, send them the current state.
-  socket.emit('update', {
+  socket.emit('welcome', {
     id: userId,
     document: text, 
     cursors: cursors
@@ -68,7 +158,7 @@ io.on('connection', function (socket) {
 
   // Add this user's cursor and announce their entry to everyone.
   cursors[userId] = 0;  // start at the top of the file
-  io.emit('user.enter', userId);
+  io.emit('connected', userId);
 });
 
 // Actually start the server. Enjoy!
